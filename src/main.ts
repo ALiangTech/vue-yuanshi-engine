5cda5bd70fe7ba37a7261ad4b94a1350c36ab34c
PiYangYang <1305544590@qq.com>
2025-06-09 17:02:30 +0800

    refactor: 重构项目结构和功能
    - 移除了不必要的文件和目录
    - 更新了路由配置和页面组件
    - 调整了样式文件和脚本- 优化了项目依赖和配置

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,33 @@
-import type { UserModule } from './types'
+import type { Ctx, UserModule } from './types'
 
-import { setupLayouts } from 'virtual:generated-layouts'
-import { ViteSSG } from 'vite-ssg'
-import { routes } from 'vue-router/auto-routes'
+import { createApp } from 'vue'
 import App from './App.vue'
+import router from './routers'
 
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
 
 // https://github.com/antfu/vite-ssg
-export const createApp = ViteSSG(
-  App,
-  {
-    routes: setupLayouts(routes),
-    base: import.meta.env.BASE_URL,
-  },
-  (ctx) => {
-    // install all modules under `modules/`
-    Object.values(import.meta.glob<{ install: UserModule }>('./modules/*.ts', { eager: true }))
-      .forEach(i => i.install?.(ctx))
-    // ctx.app.use(Previewer)
-  },
-)
+// export const createApp = ViteSSG(
+//   App,
+//   {
+//     routes: setupLayouts(routes),
+//     base: import.meta.env.BASE_URL,
+//   },
+//   (ctx) => {
+//     // install all modules under `modules/`
+//     Object.values(import.meta.glob<{ install: UserModule }>('./modules/*.ts', { eager: true }))
+//       .forEach(i => i.install?.(ctx))
+//     // ctx.app.use(Previewer)
+//   },
+// )
+const app = createApp(App).use(router)
+
+// modules 模块注册
+function registerModules(app: Ctx) {
+  Object.values(import.meta.glob<{ install: UserModule }>('./modules/*.ts', { eager: true })).forEach(i => i.install?.({ app, router }))
+}
+
+registerModules(app)
+app.mount('#app')
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,9 @@
-import type { ViteSSGContext } from 'vite-ssg'
+import type { App } from 'vue'
+import type { Router } from 'vue-router'
 
-export type UserModule = (ctx: ViteSSGContext) => void
+export type Ctx = App
+interface Params {
+  app: Ctx
+  router: Router
+}
+export type UserModule = (config: Params) => void
